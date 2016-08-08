f5d18859137cba3bd99449653d346f94b6f6f38d
GordonNY <gordonsdev@gmail.com>
2016-08-08 10:06:36 -0400

    add facebook auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808021722) do
+ActiveRecord::Schema.define(version: 20160808131043) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "description"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160808021722) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
