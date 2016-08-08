f7eab983e150ab2e75b499435cfa1332749dc5b4
GordonNY <gordonsdev@gmail.com>
2016-08-08 11:48:44 -0400

    fix show page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+	def edit_action(user_id, post)
+		if user_id && user_id == post.user_id
+    link_to 'Edit', edit_post_path(post), class: "btn btn-primary btn-xs"
+    
+    end
+	end
+
+	def delete_action(user_id, post)
+		if user_id && user_id == post.user_id
+    
+    link_to 'Delete', post, method: :delete, data: { confirm: 'Are you sure?' }, class: "btn btn-danger btn-xs"
+    end
+	end
 end
