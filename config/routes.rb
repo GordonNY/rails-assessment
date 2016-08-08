9c0642dadb5b29a8c42ab2e457aa5c0481f42677
GordonNY <gordonsdev@gmail.com>
2016-08-07 22:35:48 -0400

    add post resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  
   devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :posts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
