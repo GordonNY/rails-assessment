4e34a5170d1533c2abd6f448e6191901542b10a1
GordonNY <gordonsdev@gmail.com>
2016-08-08 11:55:21 -0400

    add comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
 	validates_presence_of :description
 
 	belongs_to :user
+	has_many :comments
 
 	def self.my_posts(user_id)
 		where(user_id: user_id).order(:created_at)
